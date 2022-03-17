fe219eb111ec875cd8f32bab11b395e993bce116
atakanreyhanioglu <atakan.reyhanioglu@gmail.com>
2022-03-17 09:55:51 +0300

    user class

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import {User} from "./models/User";
 
-const user = new User({ name: 'newrecord', age: 0 })
+const user = new User({ id: 1, name: 'newer name' })
 
-user.on('change', () => {
-    console.log('user changed html needs update')
+user.on('save', () => {
+    console.log(user)
 })
 
-user.set({ id: 1, age: 1, name: 'atakan html'})
+user.save()
 
 
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,7 @@
 import {Eventing} from "./Eventing";
 import {Sync} from "./Sync";
 import {Attributes} from "./Attributes";
+import {AxiosResponse} from "axios";
 
 export interface UserProps {
     id?: number,
@@ -25,8 +26,31 @@ export class User {
     get trigger() {
         return this.events.trigger
     }
+    get get() {
+        return this.attributes.get
+    }
     set(update: UserProps): void {
         this.attributes.set(update)
         this.events.trigger('change')
+    }
+    fetch(): void {
+        const id = this.get('id')
+        if(typeof id !== 'number') {
+            throw new Error('Cannot fetch without an id')
+        }
+
+        this.sync.fetch(id).then((res: AxiosResponse) => {
+            this.set(res.data)
+        })
+    }
+
+    save(): void {
+        this.sync.save(this.attributes.getAll())
+            .then((res: AxiosResponse): void => {
+                this.trigger('save')
+            })
+            .catch(() => {
+                this.trigger('error')
+            })
     }
 }
